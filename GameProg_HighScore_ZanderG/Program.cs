0182e3cfe4f154c98d953ae15a6ae65a717848d7
Zander Goodwin <zanderngoodwin@gmail.com>
2025-10-28 16:25:20 -0300

    Initial commit

---
+++ GameProg_HighScore_ZanderG/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProg_HighScore_ZanderG
+{
+    internal class Program
+    {
+        static Random random = new Random();
+        static bool nameLength = false;
+        static string path = "data.txt";
+        static string name;
+
+        static void Main(string[] args)
+        {
+            List<string> allFinalScore = new List<string>();
+            
+            int score = random.Next(1, 1001);
+            Console.WriteLine($"Score: {score}");
+            Thread.Sleep(1000);
+
+            while (nameLength == false)
+            {
+                Console.WriteLine("Type your initials");
+                name = Console.ReadLine();
+                
+                if (name.Length == 3)
+                {
+                    nameLength = true;
+                }
+            }
+
+            string finalScore = $"{name}: {score}";
+
+            allFinalScore.Add(finalScore);
+
+            File.WriteAllLines(path, allFinalScore);
+
+            Console.WriteLine("Press any key to end the game...");
+            Console.ReadKey(true);
+        }
+    }
+}
